ecb0b7b638d4b24fdab0bfb7c144c28428b7381e
Vanessa Toro <vanetoro84@gmail.com>
2018-05-02 14:07:41 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,7 +35,8 @@ def can_be_created_in_a_block(args={title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first.name
+  binding.pry
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
