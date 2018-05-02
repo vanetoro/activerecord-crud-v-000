8881035804cf8811f6ed057920af0c73f1d4ba71
Vanessa Toro <vanetoro84@gmail.com>
2018-05-02 14:04:10 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,6 +29,7 @@ end
 
 def can_be_created_in_a_block(args={title: "Home Alone", release_date: 1990})
   Movie.create do |m|
+    binding.pry
     m.title = args[:title]
     m.release_date = args[:release_date]
   end
