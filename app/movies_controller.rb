07e4874db3e323080a3929cfbc87d9807172126e
Vanessa Toro <vanetoro84@gmail.com>
2018-05-02 15:12:43 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -82,6 +82,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
+  binding.pry
   Movie.each_with_index(1) do |m ,i|
     m.update(i,title: 'A Movie')
   end
