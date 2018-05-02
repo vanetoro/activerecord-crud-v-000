4585f226c1f74a151f1575359dc898cfe50bb2c2
Vanessa Toro <vanetoro84@gmail.com>
2018-05-02 15:18:28 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20180501153537) do
 
-  create_table "create_movies", force: :cascade do |t|
+  create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.datetime "release_date"
     t.string   "director"
