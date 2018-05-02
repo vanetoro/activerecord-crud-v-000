93a3c3f28940b80442ca3497e7f226dda9e81431
Vanessa Toro <vanetoro84@gmail.com>
2018-05-02 14:12:06 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -46,8 +46,8 @@ def can_get_size_of_the_database
   Movie.count
 end
 
-def can_find_the_first_item_from_the_database_using_id
-  __
+def can_find_the_first_item_from_the_database_using_id(id)
+  Movie.find_by(id)
 end
 
 def can_find_by_multiple_attributes
